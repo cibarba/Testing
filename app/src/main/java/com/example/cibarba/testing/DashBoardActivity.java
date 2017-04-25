472f91bf7346bddda9b67fb1a2e12e878a96f395
strugg <ricardo.zamora123@hotmail.com>
2017-04-25 16:52:27 -0500

    25/04/17 1651

--- app/src/main/java/com/example/cibarba/testing/DashBoardActivity.java
+++ app/src/main/java/com/example/cibarba/testing/DashBoardActivity.java
@@ -1,10 +1,10 @@
 package com.example.cibarba.testing;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +15,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+
 
 public class DashBoardActivity extends AppCompatActivity implements View.OnClickListener {
 
